d427ab0b30da4e542bba82c10f292d0ba8e7ce70
itsmeabhishekn <abhishekn4454@gmail.com>
2021-11-11 20:51:29 +0530

    added routes

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ connnectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+
+//define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('server started on port ${PORT}'));
